8fe70a5ddfe5617cc9cab844c09090ea68cdb910
lengyaohui <lengyaohui.bj@acewill.cn>
2018-05-02 16:45:49 +0800

    modify

--- venv/interface_project/tmp.py
+++ venv/interface_project/tmp.py
@@ -1,9 +1,9 @@
 #_*_coding:utf-8_*_
 from interface_project.library.scripts import LogDebug
 import time
-
-
-
+import yaml
+from interface_project.globalVar import gl
+import os
 payload = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"req\"\r\n\r\n{\"cno\":\"1262797\",\"shop_id\":4101315464,\"cashier_id\":\"1196770051\",\"money\":\"100\",\"award_money2\":10,\"reward_money\":\"100\",\"is_diy\":true,\"charge_type\":1,\"remark\":\"beizhu\",\"biz_id\":\"37\",\"recommenderecode2\":9002}\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"appid\"\r\n\r\ndp1svA1gkNt8cQMkoIv7HmD1\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"ts\"\r\n\r\n1\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"sig\"\r\n\r\n1\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"v\"\r\n\r\n2.0\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
 #payload = r"------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"req\"\r\n\r\n{\"biz_id\":123455,\"send_notification2\":false,\"cashier_id\":\"1196770051\"}\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"appid\"\r\n\r\ndp1svA1gkNt8cQMkoIv7HmD1\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"ts\"\r\n\r\n1\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"sig\"\r\n\r\n1\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"v\"\r\n\r\n2.0\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
 '''
@@ -30,6 +30,33 @@ def replacePayload(old,field,val):
         LogDebug().error('%s:字段未找到.'%field)
         return old
 
+
+
+data = {"cno":"1262797","shop_id":4101315464,"cashier_id":"1196770051","money":"100","award_money2":10,"reward_money":"100","is_diy":True,"charge_type":1,"remark":"beizhu","recommenderecode2":9002,"biz_id":"37"}
+
+
+class tmpClass(object):
+    def __init__(self):
+        self.configPath =  gl.dataScenarioPath
+        self.yamlPath = os.path.join(self.configPath,'ChargeAndDeal.yaml')
+
+
+    def rndTimeStr(self):
+        curTimeStr = str(time.strftime('%Y%m%d%H%M%S', time.localtime()).encode('utf-8'))
+        return curTimeStr.decode('utf-8')
+
+    def writeYmal(self,data):
+        fp = open(self.yamlPath,'a')
+        yaml.dump(data,fp)
+
+    def getYamlfield(self):
+        f = open(self.yamlPath,'r')
+        cont = f.read()
+        ret = yaml.load(cont)
+        return ret
+
 if __name__=="__main__":
-    a = replacePayload(payload,'biz_id',456789)
-    print a
+    #a = replacePayload(payload,'biz_id',456789)
+    #print a
+    #tmpClass().writeYmal(data)
+    print tmpClass().getYamlfield()
